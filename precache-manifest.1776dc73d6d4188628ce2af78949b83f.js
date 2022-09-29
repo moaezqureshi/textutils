785a7d6c7a77fa7c25024dfa89f845f8c94b0350
Moaez Qureshi <moaez135@gmail.com>
2022-09-29 11:13:01 +0530

    Updates

---
+++ precache-manifest.1776dc73d6d4188628ce2af78949b83f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaa3a7653f46eeadb45a",
+    "url": "/textutils/static/css/main.cbaf185a.chunk.css"
+  },
+  {
+    "revision": "eaa3a7653f46eeadb45a",
+    "url": "/textutils/static/js/main.eaa3a765.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "4bb2eddc4c0f16245f5025242d703266",
+    "url": "/textutils/index.html"
+  }
+];
